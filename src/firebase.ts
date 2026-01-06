95abb3c8ba193590668d5c75c578157b3a5d278f
Kevin K R <kevinallen122451@gmail.com>
2026-01-06 15:51:03 +0530

    Bug-Fixed

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,7 +3,7 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.FIREBASE_API_KEY,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "technical-quiz-1c612.firebaseapp.com",
   projectId: "technical-quiz-1c612",
   storageBucket: "technical-quiz-1c612.firebasestorage.app",
